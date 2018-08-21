384d572773c611a7c297cdd00e3f3bbbba3254fd
Natasha Teng <nteng3290@gmail.com>
2018-08-21 17:48:14 -0400

    Added Styles and Functionality

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
+const container = document.querySelector("#reply");
+const refresh = document.querySelector('#btn-refresh');
+
 const replies = [
-    "Do what your hear desires",
+    "Do what your heart desires",
     "What are you doing with your life? That's a terrible decision",
     "Try it, doesn't hurt.",
     "Y.O.L.O"
 ]
 
-const container = document.querySelector("#reply");
-
+//Handle Form Submit
 function handleSubmit(){
     document.querySelector("#question").addEventListener("submit", function (e) {
         e.preventDefault();    //stop form from submitting
-        console.log("submitted");
         clearReplies();
         getRandomReply();
+        displayButton();
     });
 }
 
+//Clear Replies on document 
 function clearReplies(){
     if (container.innerHTML.length >= 1){
-        console.log("There is a reply");
         container.innerHTML = "";
     }
     
 }
 
+//Get Random Reply from replies array 
 function getRandomReply(){
     const randoInt = Math.floor(Math.random() * 4); 
-    const randoReply = replies[randoInt];
-    // const reply = container.createElement("p");
-    // console.log(reply);
-    container.appendChild(document.createTextNode(randoReply));
+    const randoReply = document.createTextNode(replies[randoInt]);
+    const reply = document.createElement("p");
+    reply.appendChild(randoReply);
+    container.appendChild(reply);
+}
+
+//Display refresh button after replies append to page
+function displayButton(){
+   refresh.classList.add("visible");
+   refreshAns();
+}
+
+//Handle Refresh of answers 
+function refreshAns(){
+    refresh.addEventListener("click", function (e) {
+        e.preventDefault();
+        container.innerHTML= "";
+    });
+    refresh.classList.remove("visible");
 }
 
 handleSubmit();
